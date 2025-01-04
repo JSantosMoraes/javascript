82a4022cffa4bec59df709fdbbdd81c1c8589b58
Jonas8233144234 <149276335+JSantosMoraes@users.noreply.github.com>
2025-01-04 12:57:50 -0300

    arrow function

---
+++ CFB/arrow-function/index.js
@@ -0,0 +1,24 @@
+const soma = (v1, v2) => {
+    let res  = v1 + v2
+    return res
+}
+
+/* ARROW COM UM PARÂMETRO
+
+const add=n=>n+10 << Não necessita de parênteses (SE FOR UMA OPERAÇÃO SIMPLES NÃO PRECISA DE RETURN)
+console.log(add(10))
+
+*/
+
+/* COM ARROW
+
+const soma = (v1,v2)=>{return v1 + v2} << Símbolo do arrow "=>"
+console.log(soma(10,5))
+
+*/
+
+/* FUNÇÃO ANÔNIMA COMUM
+
+const soma = function(v1,v2){return v1 + v2}
+
+*/
