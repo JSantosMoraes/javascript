9717cfa6e17a4aecd5cdd60055003b2155d2e624
Jonas8233144234 <149276335+JSantosMoraes@users.noreply.github.com>
2025-01-09 00:13:45 -0300

    ownerDocument, getRootNode

---
+++ CFB/child,children e parents/index.js
@@ -0,0 +1,11 @@
+const caixa1 = document.querySelector('#caixa1')
+const btn_c = [...document.querySelectorAll('.curso')]
+
+console.log(document.getRootNode())
+console.log(document.ownerDocument())
+
+/*
+console.log(caixa1.firstElementChild)
+console.log(caixa1.lastElementChild)
+console.log(caixa1.children)
+*/
