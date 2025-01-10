26e3443badceff5830418985ac8cf420f962ecf6
Jonas8233144234 <149276335+JSantosMoraes@users.noreply.github.com>
2025-01-09 23:02:12 -0300

    parentNode

---
+++ CFB/condição-ternária/index.js
@@ -0,0 +1,8 @@
+const caixa1 = document.querySelector('#caixa1')
+const btn_c = [...document.querySelectorAll('.curso')]
+
+console.log(caixa1.hasChildNodes())
+console.log(btn_c[0].hasChildNodes())
+console.log(btn_c[0].childNodes)
+
+console.log(caixa1.children.length > 0 ? 'Possui filhos' : 'Não possui filhos')
