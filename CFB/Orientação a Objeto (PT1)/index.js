d6b887a2cb2ad06a5c33bc6391a234aadc83e2f2
Jonas8233144234 <149276335+JSantosMoraes@users.noreply.github.com>
2025-01-29 17:08:49 -0300

    Orientação a Objeto (PT1)

---
+++ CFB/Orientação a Objeto (PT1)/index.js
@@ -0,0 +1,29 @@
+class Pessoa{
+  constructor(pnome){
+    this.nome = pnome
+  }
+}
+
+let p1 = new Pessoa('Bruno')
+let p2 = new Pessoa('Asdervardson')
+let p3 = new Pessoa('Brenasclartel')
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
+
+/*
+class Pessoa{
+  constructor(){
+    this.nome = 'Bruno'
+  }
+}
+
+let p1 = new Pessoa()
+let p2 = new Pessoa()
+let p3 = new Pessoa()
+
+console.log(p1.nome)
+console.log(p1.nome)
+console.log(p1.nome)
+*/
