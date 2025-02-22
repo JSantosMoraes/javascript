c89cd15f25403ea550b06e48a951db79a74d7cd1
Jonas8233144234 <149276335+JSantosMoraes@users.noreply.github.com>
2025-02-21 22:57:15 -0300

    Exercício

--- CFB/Exercício sobre Classes (PT1)/index.js
+++ CFB/Exercício sobre Classes (PT1)/index.js
@@ -1,3 +1,65 @@
+// Tipo
+const f_tipoMilitar = document.querySelector("#f_tipoMilitar")
+const f_tipoNormal = document.querySelector("#f_tipoNormal")
+// Atributos
+const f_blindagem = document.querySelector("#f_blindagem")
+const f_municao = document.querySelector("#f_municao")
+const f_nome = document.querySelector("#f_nome")
+const f_portas = document.querySelector("#f_portas")
+// divs
+const div_carros = document.querySelector("#carros")
+const btn_addCarro = document.querySelector("#btn_addCarro")
+
+let a_carros = []
+
+f_tipoMilitar.addEventListener("click", (evt) => { // Radio Militar
+    f_nome.value = ''
+    f_portas.value = 0
+    f_blindagem.value = 0
+    f_municao.value = 0
+    f_blindagem.removeAttribute("disabled")
+    f_municao.removeAttribute("disabled")
+})
+
+f_tipoNormal.addEventListener("click", (evt) => { // Radio Normal
+    f_nome.value = ''
+    f_portas.value = 0
+    f_blindagem.value = 0
+    f_municao.value = 0
+    f_blindagem.setAttribute("disabled", "disabled")
+    f_municao.setAttribute("disabled", "disabled")
+});
+
+const gerenciarExibicaoCarros = () => {
+    div_carros.innerHTML = ""
+    a_carros.forEach((c) => {
+        const div = document.createElement("div")
+        div.setAttribute("class", "carro")
+        div.innerHTML = `<strong>Nome:</strong> ${c.nome}<br/>`
+        div.innerHTML += `<strong>Portas:</strong> ${c.portas}<br/>`
+        div.innerHTML += `<strong>Cor:</strong> ${c.cor}<br/>`
+        div.innerHTML += `<strong>Blindagem:</strong> ${c.blindagem}<br/>`
+        div.innerHTML += `<strong>Munição:</strong> ${c.municao}<br/>`
+        div_carros.appendChild(div)
+    })
+};
+
+btn_addCarro.addEventListener("click", () => {
+    div_carros.setAttribute('class','carros')
+    if (f_tipoNormal.checked) {
+        const c = new Carro(f_nome.value,f_portas.value)
+        a_carros.push(c)
+    } else {
+        const c = new Militar(f_nome.value,f_portas.value,f_blindagem.value,f_municao.value)
+        a_carros.push(c)
+    }
+    gerenciarExibicaoCarros()
+    f_nome.value = ''
+    f_portas.value = 0
+    f_blindagem.value = 0
+    f_municao.value = 0
+})
+
 class Carro { // Classe PAI // Classe Base
     constructor(nome,portas){
         this.nome = nome
