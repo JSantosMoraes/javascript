1d779b06f138c06b564ec5ed0c5d8348954cf570
Jonas8233144234 <149276335+JSantosMoraes@users.noreply.github.com>
2025-03-03 10:50:49 -0300

    Aprendendo Mais Sobre JSON ( Por fora do CFB Cursos )

---
+++ CFB/JSON ( Por fora )/js/script.js
@@ -0,0 +1,25 @@
+const objs = [{
+    nome: 'Matheus',
+    idade: 30,
+    esta_trabalhando: true,
+    detalhes_profissao:{
+        profissao: 'Programador',
+        empresa: 'Empresa X'
+    },
+    hobbies:['Programar','Correr','Ler']
+},
+{
+    nome: 'João',
+    idade: 25,
+    esta_trabalhando: false,
+    detalhes_profissao:{
+        profissao: null,
+        empresa: null
+    },
+    hobbies:['Jogar','Academia']
+},
+]
+
+const jsonData = JSON.stringify(objs) // Convertendo o objeto para JSON
+
+console.log(jsonData)
